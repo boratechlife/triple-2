75934ae37553d67ca35adb970e2eeff31d08954f
Dennis Beemsterboer <dbeemsterboer@me.com>
2023-12-15 08:44:12 +0000

    Updated 1 file via CloudCannon.

--- src/components/contact/form/form.jsx
+++ src/components/contact/form/form.jsx
@@ -52,7 +52,7 @@ export default function ContactForm(block) {
           <div className="col-lg-6 order-1 order-lg-2">
           
             <div className="contact-form">
-            <div className="calendly-inline-widget" data-url="https://calendly.com/tripleblue/demo-ai-notulistensoftware-vve" style={{ minWidth: "320px", height: "700px" }}></div>
+            <div className="calendly-inline-widget" data-url="https://calendly.com/bram-tripleblue/30min-demo" style={{ minWidth: "320px", height: "700px" }}></div>
             <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
               
               
